8e5b5235269cd706ab1b28e09ea65096428a782f
Terrunda <terrencegift49@gmail.com>
2025-10-05 22:30:44 +0300

    Added some methods to each constructor that will be used later

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 function TaskConstructor() {
+    this.TaskID = Date.now();
     this.TaskName = TaskName;
-    this.DueDate = Date.now()
+    this.DueDate =DueDate;
     this.TaskComplete = false;
     this.TaskNote = TaskNote;
     this.EarlyReminder = null;
+
+TaskConstructor.prototype.ToggleComplete = function() {
+    this.TaskComplete = true;
+};
 } 
 
 
 function ListConstructor() {
     this.ListName = ListName;
     this.TasksInList = [];
+
+ListConstructor.prototype.FindTask = function(TaskName) {
 }
+
+ListConstructor.prototype.DeleteTask = function() {
+
+}
+
+ListConstructor.prototype.addTask = function(taskInstance) {
+    this.TasksInList.push(taskInstance)
+}
+}
+
